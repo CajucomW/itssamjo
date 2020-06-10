9f879beaaa7ad3b423a78825018d0129b52b8b05
Wence Cajucom <cajucom.w@gmail.com>
2020-06-10 06:50:43 -0700

    modified: views.py

--- itssamjo/blog/views.py
+++ itssamjo/blog/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import BlogModel
+from .serializers import *
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def blog_list(request):
+    if request.method == 'GET':
+        data = BlogModel.objects.all()
+        serializer = BlogModelSerializer(
+            data, 
+            context={'request': request}, 
+            many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BlogModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def blog_detail(request, pk):
+    try:
+        blog = BlogModel.objects.get(pk=pk)
+    except BlogModel.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PUT':
+        serializer = BlogModelSerializer(
+            blog,
+            data=request.data,
+            context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
