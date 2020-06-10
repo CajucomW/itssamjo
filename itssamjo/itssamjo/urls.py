44441232a79842afebc1b50814d0010088d032ba
Wence Cajucom <cajucom.w@gmail.com>
2020-06-10 07:10:11 -0700

    backend mostly done w/ exception of variable url. started frontend.

--- itssamjo/itssamjo/urls.py
+++ itssamjo/itssamjo/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/blog', views.blog_list),
+    path('api/bloglist', views.blog_list),
     path('api/blog/<int:pk>', views.blog_detail),
 ]
