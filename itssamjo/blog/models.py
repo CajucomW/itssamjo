b4f397d31d74430d7b6a9385fc1329808e047ab1
Wence Cajucom <cajucom.w@gmail.com>
2020-06-16 06:47:44 -0700

    created new branch 'login'. moved env to 'itssamjo/'

--- itssamjo/blog/models.py
+++ itssamjo/blog/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class BlogModel(models.Model):
     title = models.CharField("Title", max_length=240)
     text = models.TextField()
+    # image = models.FileField(upload_to="blogimages/", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
-    return self.title | self.created | self.text
+    return self.title + ' said ' + self.text
