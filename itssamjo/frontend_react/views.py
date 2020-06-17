d19136f3ad768789bedb9b2cec0eb16d30a06c72
Wence Cajucom <cajucom.w@gmail.com>
2020-06-16 22:34:13 -0700

    need to redo frontend/react App w/in django

--- itssamjo/frontend_react/views.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'frontend_react/index.html')
