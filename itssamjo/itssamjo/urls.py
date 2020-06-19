7d72349de12813c4361804324306c789a3577d2f
Wence Cajucom <cajucom.w@gmail.com>
2020-06-19 06:53:41 -0700

    modified urls.py in backend to load REACT links when refreshed

--- itssamjo/itssamjo/urls.py
+++ itssamjo/itssamjo/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reactfrontend.urls')),
+    path('blog', include('reactfrontend.urls')),
+    path('write-blog', include('reactfrontend.urls')),
     path('', include('blog.urls')),
     path('api/blog', views.blog_list),
     # path('api/blog/<int:pk>', views.blog_detail)
