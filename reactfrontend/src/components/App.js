54a168d285a9a2b233bc3505ff40b8c248cd8b31
Wence Cajucom <cajucom.w@gmail.com>
2020-06-24 07:09:04 -0700

    drop deployment for now and move forward. address deployment when closer to production.

--- reactfrontend/src/components/App.js
+++ reactfrontend/src/components/App.js
@@ -1,7 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
 import {
     BrowserRouter as Router,
     Switch,
@@ -16,29 +14,21 @@ import WriteBlog from './WriteBlog.js';
 import { Provider } from 'react-redux';
 import store from '../store';
 
-// React Alerts
-const alertOptions = {
-    timeout: 3000,
-    position: 'top center'
-}
-
 class App extends Component {
     render() {
         console.log('=====App=====');
         return (
             <Provider store={store}>
-                <AlertProvider>
-                    <Router>
-                        <Header />
-                        <div>
-                            <Switch>
-                                <Route exact path='/' component={Home} />
-                                <Route exact path='/blog' component={Blog} />
-                                <Route exact path='/write-blog' component={WriteBlog} />
-                            </Switch>
-                        </div>
-                    </Router>
-                </AlertProvider>
+                <Router>
+                    <Header />
+                    <div>
+                        <Switch>
+                            <Route exact path='/' component={Home} />
+                            <Route exact path='/blog' component={Blog} />
+                            <Route exact path='/write-blog' component={WriteBlog} />
+                        </Switch>
+                    </div>
+                </Router>
             </Provider>
         );
     }
