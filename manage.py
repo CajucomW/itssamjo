a28b035941989e67f025fbbd20e21fe78ebc4b14
Wence Cajucom <cajucom.w@gmail.com>
2020-07-20 20:29:45 -0700

    edited gitignore and manage.py. removed asgi.py code to use wsgi.py again

--- manage.py
+++ manage.py
@@ -4,7 +4,23 @@ import os
 import sys
 
 
-def main():
+# def main():
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itssamjo.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
+
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itssamjo.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,7 +31,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
